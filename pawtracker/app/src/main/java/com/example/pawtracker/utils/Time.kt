1d7f6679548dad8e1871274649894009df8ca4dd
emmkor <emmi.korhonen4@metropolia.fi>
2026-04-05 23:08:15 +0300

    timestamp utils for for date-based filtering

---
+++ pawtracker/app/src/main/java/com/example/pawtracker/utils/Time.kt
@@ -0,0 +1,25 @@
+package com.example.pawtracker.utils
+
+import java.util.Calendar
+
+object TimeUtils {
+
+    fun getStartOfDay(): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar.timeInMillis
+    }
+
+    fun getStartOfWeek(): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar.timeInMillis
+    }
+}
