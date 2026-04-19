3f417c4f2139beac1a17b98abf943a6d8db6d97c
emmkor <emmi.korhonen4@metropolia.fi>
2026-04-19 05:26:00 +0300

    Add repository data into statistics ui state -unit test

--- pawtracker/app/src/test/java/com/example/pawtracker/StatisticsViewModelTest.kt
+++ pawtracker/app/src/test/java/com/example/pawtracker/StatisticsViewModelTest.kt
@@ -24,6 +24,7 @@ import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import org.junit.Assert.assertEquals
 
+
 @OptIn(ExperimentalCoroutinesApi::class)
 class StatisticsViewModelTest {
 
@@ -40,105 +41,27 @@ class StatisticsViewModelTest {
     }
 
     @Test
-    fun `uiState combines all flows correctly`() = runTest {
+    fun `combines repository data into statistics ui state`() = runTest {
 
         val viewModel = StatisticsViewModel(
             FakeWalkRepository(),
             FakeDogProfileRepository()
         )
 
-        advanceUntilIdle()
-
-        val state = viewModel.uiState.first()
-
-        assertEquals(2f, state.todayDistance)
-        assertEquals(100L, state.todayDuration)
-        assertEquals(10f, state.weekDistance)
-        assertEquals(500L, state.weekDuration)
-
-        assertEquals(5f, state.goalDistance)
-        assertEquals(30L, state.goalDuration)
-
-        assertEquals("Nelli", state.dogName)
-        assertEquals("testUri", state.imageUri)
-    }
-}
-
-
-/*
-@OptIn(ExperimentalCoroutinesApi::class)
-class StatisticsViewModelTest {
-
-    private val walkRepository = mockk<WalkRepository>()
-    private val profileRepository = mockk<DogProfileRepository>()
-
-    @Test
-    fun `uiState combines flows correctly`() = runTest {
-
-        // STUB FLOWIT
-        every { walkRepository.getTodayDistance() } returns flowOf(2f)
-        every { walkRepository.getTodayDuration() } returns flowOf(100L)
-        every { walkRepository.getWeekDistance() } returns flowOf(10f)
-        every { walkRepository.getWeekDuration() } returns flowOf(500L)
-
-        every { profileRepository.getProfile() } returns flowOf(
-            DogProfileEntity(
-                name = "Nelli",
-                imageUri = "TestUri",
-                dailyDistanceGoal = 5f,
-                dailyDurationGoal = 30L
-            )
-        )
-
-        val viewModel = StatisticsViewModel(walkRepository, profileRepository)
-
-        val state = viewModel.uiState.first()
-
-        assertEquals(2f, state.todayDistance)
-        assertEquals(100L, state.todayDuration)
-        assertEquals(10f, state.weekDistance)
-        assertEquals(500L, state.weekDuration)
-
-        assertEquals("Nelli", state.dogName)
-        assertEquals("TestUri", state.imageUri)
-    }
-}
-
-
-// Test option 3
-@OptIn(ExperimentalCoroutinesApi::class)
-class StatisticsViewModelTest {
-
-    private val walkRepository = mockk<WalkRepository>()
-    private val profileRepository = mockk<DogProfileRepository>()
-
-    @Test
-    fun `uiState combines flows correctly`() = runTest {
-
-        every { walkRepository.getTodayDistance() } returns flowOf(2f)
-        every { walkRepository.getTodayDuration() } returns flowOf(100L)
-        every { walkRepository.getWeekDistance() } returns flowOf(10f)
-        every { walkRepository.getWeekDuration() } returns flowOf(500L)
-
-        every { profileRepository.getProfile() } returns flowOf(
-            DogProfileEntity(
-                name = "Nelli",
-                imageUri = "testUri",
-                dailyDistanceGoal = 5f,
-                dailyDurationGoal = 30L
-            )
-        )
-
-        val viewModel = StatisticsViewModel(walkRepository, profileRepository)
-
         viewModel.uiState.test {
 
+            awaitItem()
             val state = awaitItem()
+
+            advanceUntilIdle()
 
             assertEquals(2f, state.todayDistance)
             assertEquals(100L, state.todayDuration)
             assertEquals(10f, state.weekDistance)
             assertEquals(500L, state.weekDuration)
+
+            assertEquals(5f, state.goalDistance)
+            assertEquals(30L, state.goalDuration)
 
             assertEquals("Nelli", state.dogName)
             assertEquals("testUri", state.imageUri)
@@ -147,4 +70,3 @@ class StatisticsViewModelTest {
         }
     }
 }
-*/
